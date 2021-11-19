9edc99f47839d653b133f9271cb49a7b46d48af3
tuncerrstm <tuncerrstm@gmail.com>
2021-11-20 00:28:05 +0300

    listing of comments
    listing of comments (32)

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username = "Tuncer"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username = "Muazes"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username = "Müge"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
